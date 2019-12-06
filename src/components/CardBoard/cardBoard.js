0fd2c21496793e0e948cb5fe04fb623902b252ef
Jeffrey Huddleston <jacobjhudd@gmail.comgit config --global user.email jacobjhudd@gmail.comgit config --global user.email jacobjhudd@gmail.com>
2019-12-05 22:25:36 -0500

    added wrapper

---
+++ src/components/CardBoard/cardBoard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CardBoard() {
+    return (
+        <header>
+            <p>The game goes here</p>
+        </header>
+    )
+}
+
+export default CardBoard;
---
+++ src/components/Footer/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <header>
+            <p>This is a footer</p>
+        </header>
+    )
+}
+
+export default Footer;
